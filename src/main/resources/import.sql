57ec302d0ec0f919ae0c613c9a185bde36931d1d
Gilcecler Carneiro <gilcecler@gmail.com>
2023-12-21 19:54:11 -0300

    criei um user somente admin pra testar, no outro commit já tinha o salvar pedido

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -58,6 +58,7 @@ INSERT INTO tb_product_category (product_id, category_id) VALUES (25, 3);
 
 INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Maria Brown', 'maria@gmail.com', '988888888', '$2a$10$wtATb6BVXocao8fBUiy0yOkKUxzknMvjjE8Bbur1V/V07xupwlVqC', '2001-07-25');
 INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Alex Green', 'alex@gmail.com', '977777777', '$2a$10$wtATb6BVXocao8fBUiy0yOkKUxzknMvjjE8Bbur1V/V07xupwlVqC', '1987-12-13');
+INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Admin', 'admin@admin.com', '977777777', '$2a$10$wtATb6BVXocao8fBUiy0yOkKUxzknMvjjE8Bbur1V/V07xupwlVqC', '1987-12-13');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_CLIENT');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
@@ -65,6 +66,7 @@ INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 
 
 
